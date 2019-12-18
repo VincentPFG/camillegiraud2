c68f05592609f68b54cc96482c2675bbb08ff682
VincentPFG <vincentgiraud@live.fr>
2019-12-18 14:39:44 +0100

    test form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,7 +7,7 @@ div
         //-     v-layout(justify-center)
         //-         v-btn(v-on='on') contacter
         v-card
-            v-form(netlify name='contact' action='')
+            v-form(netlify name='contact')
                 div(hidden)
                     input(name='form-name' value='contact')
                     input(name='de' :value='intro')
@@ -23,7 +23,7 @@ div
                             v-text-field(v-model='email' label='Mail')
                     v-textarea(v-model='message' label='Message')
                     v-row(justify='center')
-                        v-btn(type='submit' fab color='primary')
+                        v-btn(@click='submit' fab color='primary')
                             v-icon mdi-send
 </template>
 
