4ac8d39d0ca6e0680db380ed27490dba3e06fc0a
VincentPFG <vincentgiraud@live.fr>
2019-12-18 14:08:28 +0100

    test form

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -7,7 +7,7 @@ div
         //-     v-layout(justify-center)
         //-         v-btn(v-on='on') contacter
         v-card
-            v-form(netlify name='contact')
+            v-form(netlify name='contact' action='')
                 div(hidden)
                     input(name='form-name' value='contact')
                     input(name='de' :value='intro')
